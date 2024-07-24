8b4c50b589e501c2101d2e257956bc74ff0d395a
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-07-24 19:02:38 +0500

    commit

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import shutil
 bot = Bot("5904607271:AAH-edy50mxak7BhgfeCB-9oLnlrK5QMPiM")
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.answer(f"Salom {message.from_user.full_name}")
-
 
 @dp.message_handler()
 async def mainpart(message: types.Message):
