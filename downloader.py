2bfd1a984bd42d4040a2b47996142a89b9869462
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-07-24 21:14:44 +0500

    commit

--- downloader.py
+++ downloader.py
@@ -3,16 +3,19 @@ import os
 import glob
 
 def InstagramDownloader(url):
-    loader = instaloader.Instaloader()
-    shortcode = url.split('/')[-2]
-    post = instaloader.Post.from_shortcode(loader.context, shortcode)
-    loader.download_post(post, post.owner_username)
-    patter = os.path.join(post.owner_username, "*.txt")
-    video = os.path.join(post.owner_username, "*.mp4")
-    txt_file = glob.glob(patter)
+    L = instaloader.Instaloader()
+    post_url = url
+    shortcode = post_url.split('/')[-2]
+    post = instaloader.Post.from_shortcode(L.context, shortcode)
+    L.download_post(post, target=post.owner_username)
+    pattern = os.path.join(post.owner_username, '*.txt')
+    video = os.path.join(post.owner_username, '*mp4')
+    txt_file = glob.glob(pattern)
     video_file = glob.glob(video)
-    return [txt_file[0], video_file[0], post.owner_username]
+    return [txt_file[0], video_file, post.owner_username]
 
-video = InstagramDownloader('https://www.instagram.com/p/C9FpLIssdFd/?utm_source=ig_web_copy_link')
+video = InstagramDownloader("https://www.instagram.com/reel/C9zZgMmuMkS/?utm_source=ig_web_copy_link")
 
-print(video)
+print(video[0])
+
+os.system(f"start {video[1][0]}")
